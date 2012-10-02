ee800e6277acde72062d76ff6687f20f69a3d808
Derek Wormdahl <derekwormdahl@comcast.net>
2012-10-01 22:10:10 -0700

    updates

--- opl.py
+++ opl.py
@@ -3,7 +3,19 @@ from bs4 import *
 import urllib2
 from decimal import *
 from datetime import datetime
-from bs4 import NavigableString 
+from bs4 import NavigableString
+import collections
+
+def convert(data):
+    if isinstance(data, unicode):
+        return str(data)
+    elif isinstance(data, collections.Mapping):
+        return dict(map(convert, data.iteritems()))
+    elif isinstance(data, collections.Iterable):
+        return type(data)(map(convert, data))
+    else:
+        return data
+ 
 
 doc = BeautifulSoup(urllib2.urlopen("http://www.oregonpremierleague.com/schedules/Fall2012/47896539.html","html5lib"));
 
@@ -37,16 +49,29 @@ t = doc.find("table").find(id="tblListGames2").find("tbody")
 
 print "*********************************************************"
 #print t.tr.next_sibling.next_sibling.next_sibling.next_sibling
+
 for sib in t.tr.next_siblings:
 	if not isinstance(sib, NavigableString):
 	#if(type(sib) is bs4.element.Tag):
 	#if(callable(sib.select)):
 		if(sib.select(".RowHeader")):
-			print "Found Row Header"
+			print "****** Game Day: *****",sib.td.text.strip()
 		#else:
-		#	print "************ ",sib
-		if(sib(class_="sch-main-gm")):
-			print "    game"#,g.td.span.text
+		# print "************ ",sib
+		t = convert(sib.get("class",''))
+		if isinstance(t,list):
+			if(t.index('sch-main-gm') > 0):
+				print sib
+		#for tt in t:
+		#	print tt
+		"""
+		t1 = convert(sib.attrs)
+		if t1.has_key('class'):
+			print t1['class']
+			"""
+		#print 'class' in sib.attrMap
+		#if((sib.get("class")).contains("sch-main-gm")):
+		#	print "    game"#,g.td.span.text
 	#print "Sib: ",sib," Type: ",type(sib)
 
 
